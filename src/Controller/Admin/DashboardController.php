78ba11d7b8a9b5b6eb0b46eda78cd818b29906b5
Benj <benbourgeois@hotmail.fr>
2022-09-02 13:45:40 +0200

    debug redirect admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,9 +18,10 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        $routeBuilder = $this->get(AdminUrlGenerator::class);
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
 
-        return $this->redirect($routeBuilder->setController(CommandeCrudController::class)->generateUrl());
+        // Option 1. Make your dashboard redirect to the same page for all users
+        return $this->redirect($adminUrlGenerator->setController(CommandeCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
