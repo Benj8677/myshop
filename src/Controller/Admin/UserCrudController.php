e44c355fa51859e14745647c5ad63d48fd8e228a
Benj <benbourgeois@hotmail.fr>
2022-09-02 13:57:39 +0200

    ajout hash password admin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,14 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }    
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -41,4 +50,14 @@ class UserCrudController extends AbstractCrudController
         $user->setDateEnregistrement(new \DateTime);
         return $user;
     }
-}
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance->getId()) {
+            $entityInstance->setPassword(
+                $this->hasher->hashPassword($entityInstance, $entityInstance->getPassword())
+            );
+        }
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
+    }}
